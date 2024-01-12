8a53050b63b20c80a4b71fd4b377f8a4c1ec990f
raido-org <147738897+raido-org@users.noreply.github.com>
2024-01-12 18:13:38 +0100

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,14 +1,206 @@
 var cards = [
 	[
-    "Cindy er 18 år gammel. Hun er ordblind og diagnosticeret med ADHD.<br><br>Hun er dygtig, når hun samler sig, om det hun skal, men hun er ofte ukoncentreret. Hun er meget aktiv og social på skolehjemmet om aftenen (og om natten), og hun er derfor ofte træt og uoplagt om dagen.",
-    "<i>Tror I Julie har en diagnose? Hvis ja, hvilken?<br><br>Hvad ville I gøre, hvis I var Julies mentor?<br><br>Er der noget, man kan gøre for at hjælpe klassekammeraterne?</i>",
-    1,
-    "SKOLE"
+"Cindy er 18 år gammel. Hun er ordblind og diagnosticeret med ADHD.<br><br>Hun er dygtig, når hun samler sig, om det hun skal, men hun er ofte ukoncentreret. Hun er meget aktiv og social på skolehjemmet om aftenen (og om natten), og hun er derfor ofte træt og uoplagt om dagen.",
+"<i>Hvordan hjælper vi dobbeltudfordrede elever som Cindy i dag? <br>Hvordan samarbejder mentor, underviser, læsevejleder i praksis?<br>Er der noget, vi kan ændre for at give bedre støtte til elever som Cindy?<br><i>",
+1,
+"category"
 ],
 [
-    "Casper er i gruppe med Sara, der ikke taler særlig godt dansk.<br><br>Han vil gerne hjælpe hende, men er i tvivl om, hvordan han skal gøre det - og om Sara vil have hjælp.",
-    "<i>Hvad kan Casper gøre?<br><br>Hvordan kan I hjælpe elever med sproglige udfordringer på jeres hold?</i>",
-    2,
-    "SKOLE"
+"25-årige Julie har svært ved at forstå fysiske og sociale grænser. Hun stirrer på de andre piger, når de klæder om, og hun opsøger ofte omklædningsrummet, selvom hun ikke selv skal klæde om. <br><br>I hendes klasse er eleverne generelt fysiske fx i form af kram, men Julie overgør det i en grad, hvor det bliver ubehageligt for de andre. De ved ikke, hvordan de siger fra og begynder at trække sig fra hende.",
+"<i>Tror I Julie har en diagnose? Hvis ja, hvilken?<br>Hvad ville I gøre, hvis I var Julies mentor?<br>Er der noget, man kan gøre for at hjælpe klassekammeraterne? <br><i>",
+2,
+"category"
 ],
+[
+"Stine er 23 år gammel og har generaliseret angst. Hun har meget fravær og klarer sig ikke godt i skolen.<br><br>Dog går det meget bedre, når Stine er ude på lærepladsen, hvor hun trives og får ros.",
+"<i>Kan I genkende situationen fra andre elever?<br>Har I bud på, hvorfor der er forskel på, hvor Stine trives?<br>Hvordan kan man hjælpe hende socialt og fagligt i skolen?<br><i>",
+3,
+"category"
+],
+[
+"Emil er 17 år og har flere diagnoser, er selvmordstruet og har et stofmisbrug. <br><br>Hans mentor mener ikke, at det er forsvarligt at have ham boende på skolehjemmet, da hun er bange for, at han kan finde på at gøre skade på sig selv. Hun ved samtidigt ikke, hvor hun skal søge hjælp, og hvordan hun hjælper Emil bedst.",
+"<i>Hvem kontakter man, hvis en elev vil gøre skade på sig selv eller andre?<br>Er der behov for en fælles retningslinje i mentorteamet om dette? Hvordan støtter man bedst mentees som Emil?<br>Emil er 17 år og har flere diagnoser, er selvmordstruet og har et stofmisbrug. <br><i>",
+4,
+"category"
+],
+[
+"Ahmed er 22 år. Han har ingen diagnoser og får ikke SPS i dag. <br><br>Han er dog både socialt og fagligt meget udfordret på grundforløbet. Hans underviser tror ikke, at han kan gennemføre medmindre skolen handler og gør noget snart. <br><br>En mentor som har set Ahmed i undervisningen er enig med underviser, men de kan se, at han ikke er blevet tildelt SPS ved skolestart.",
+"<i>Kan du genkende situationen fra elever, du har mødt? <br>Hvordan afklarer skolen om elever uden en diagnose kan modtage SPS?<br>Hvad foreslår du, at mentor og underviser gør ved Ahmets situation?<br><i>",
+5,
+"category"
+],
+[
+"Fatima er en 25-årig kvinde med diagnosen paranoid skizofreni. Hun er velmedicineret, og dagligdagen på skolen fungerer godt. <br><br>Fatimas mentor kender ikke diagnosen, og han er i tvivl om, hvordan han bedst støtter hende. Han mener at have læst engang, at mennesker med paranoid skizofreni kan være utilregnelige, og han kan ikke slippe tanken, om hun pludselig kan skade ham eller sig selv.",
+"<i>Hvad ved I om paranoid skizofreni? Har I mødt elever med diagnosen? <br>Hvilke typer udfordringer kan mentorer ikke hjælpe Fatima med? Hvorfor?<br>Har du oplevet at være utryg ved en elev? Hvad er gode råd til mentorer?<br><i>",
+6,
+"category"
+],
+[
+"Ulrik er 26 år og har social angst. Han er nervøs for at skulle have en læreplads, da han forventer, at der vil være mange nye mennesker. <br><br>Det kan være vanskeligt at aflæse Ulrik, og svært at finde ud af, om han er i stand til at arbejde på en læreplads på egen hånd, eller om han skal have tæt kontakt med en mentor på lærepladsen.",
+"<i>Hvordan sker der med mentorstøtten mellem skole og læreplads i dag?<br>Hvordan støtter man mentees, som er nervøse for at få en læreplads?<br>Hvornår vil I anbefale, at mentorstøtten fortsætter under lærepladsen? <br><i>",
+7,
+"category"
+],
+[
+"Mustafa er 22 år og har ADHD. Hans mentor tror, at han har flere diagnoser. <br><br>Mustafa er udadreagerende, og han har en tendens til at være fysisk voldelig, når han bliver frustreret. Han har både truet sin underviser og sin mentor. Det sker ofte i forbindelse med, at han har svært ved at forstå en opgave.",
+"<i>Hvordan kan man forebygge, at elever som Mustafa bliver voldelige?<br>Hvad gør man, hvis en elev bliver udadreagerende over for andre elever? <br>Hvordan samarbejder mentor, underviser, læsevejleder i praksis?<br><i>",
+8,
+"category"
+],
+[
+"Jens er 17 år gammel og har diagnosen borderline. Han har brug for meget struktur og mange gentagelser for at kunne følge med i og få noget ud af undervisningen.<br><br>Jens reagerer med vrede og aggressiv adfærd, når han ikke forstår opgaven. Han fylder i klassen, da han kræver meget opmærksomhed. Samtidig påvirker hans adfærd andre sårbare elever, der fx har angst.",
+"<i>Hvordan kan man hjælpe elever med at få overblik over deres opgaver?<br>Hvad kan man gøre for at forebygge denne problematik? <br>Er der noget, man kan gøre for at hjælpe Jens og klassekammeraterne? <br><i>",
+9,
+"category"
+],
+[
+"Clara er 20 år gammel og har ingen formelle diagnoser. Hun fortæller dog ofte, at hun har angst og har højt fravær. <br><br>Clara dukker sjældent op, når hun skal fremlægge, hvilket går ud over hende selv og klassekammeraterne.",
+"<i>Hvad er tegn på, at elever har angst som diagnose? Hvilke former findes?<br>Har I 2-3 gode råd til, hvordan man bedst støtter elever med angst?<br>Hvordan gør man klassefællesskabet til et trygt rum for alle elever?<br><i>",
+10,
+"category"
+],
+[
+"Frederik er 17 år og har ADHD. Han har svært ved at komme i gang med sine opgaver. Han er med på værkstedet, deltager i samtaler og stiller spørgsmål i timerne. <br><br>Men når han selv skal yde noget aktivt, så går han i stå. Frederiks underviser synes, at hun har prøvet alt og ved ikke længere, hvad hun skal stille op.",
+"<i>Kan I genkende situationen? Hvad gjorde I for at støtte eleven?<br>Hvad kan man generelt gøre for at støtte elever som Frederik?<br>Er der noget mentor kan gøre for at støtte mentee og evt. underviseren?<br><i>",
+11,
+"category"
+],
+[
+"Sasha er 20 år gammel. Skolen kender ikke til hendes diagnoser. Når hun er på skolen går det fint, men i perioder dukker hun ikke op til timerne, og kan være umulig at komme i kontakt med. <br><br>Underviser oplever, at Sasha har et lavt selvværd og gætter på, at hun har et hashmisbrug.",
+"<i>Hvilke redskaber kan man bruge til at engagere elever med højt fravær?<br>Hvad gør man, når man fornemmer, at en elev har et misbrug?<br>Hvornår skal man tage fat i skolens psykoterapeut for at få hjælp?<br><i>",
+12,
+"category"
+],
+[
+"Poul er 20 år og har autisme. Han siger tit, at det går godt, og at han ikke har nogen problemer. Han har dog - som den eneste i klassen - ikke fået søgt om en læreplads i tide.<br><br>Han vil gerne være som alle de andre, og han har svært ved at erkende og gøre noget ved, når har brug for hjælp.",
+"<i>Hvordan støtter man bedst elever i processen med at søge en læreplads?<br>Hvordan opdager man dem, som ikke selv siger, at de har brug for hjælp?<br>Hvordan hjælper man elever, som ikke erkender, at de har brug for hjælp?<br><i>",
+13,
+"category"
+],
+[
+"Sofie på 20 år har autisme. Sofie er stille og indadvendt, og hun undgår så vidt muligt alle former for gruppearbejde. Når hun ikke kan undgå gruppearbejdet kan hun blive helt passiv uden at sige noget.<br><br>Når hun engang i mellem er aktiv i gruppearbejdet ignorerer hun ofte, hvad de andre siger og står meget fast på sin egen overbevisning fx om hvordan en problemstilling skal løses.",
+"<i>Har I 2-3 råd til, hvordan man bedst skaber det gode gruppearbejde?<br>Hvordan kan man motivere elever som Sofie til at indgå i gruppearbejde?<br>Hvordan afgør man, hvem der (ikke) kan indgå i gruppearbejde sammen?<br><i>",
+14,
+"category"
+],
+[
+"Kaj er 27 år og har ADD. Han har svært ved at danne sig et overblik over, hvad han skal i løbet af ugen. Han har altid haft svært ved at koncentrere sig og bliver hurtig træt. <br><br>Kaj har en søn på 3 år, der kræver meget opmærksomhed, og Kaj oplever ofte, at han ikke har energi til at forberede sig til skolen og få lavet sine lektier.",
+"<i>Hvordan plejer I at hjælpe mentees, som har svært ved at få lavet lektier?<br>Hvordan hjælper man mentees med at få skabt struktur over hverdagen?<br>Har I 2-3 gode råd til, hvordan man bedst støtter mentees med børn?<br><i>",
+15,
+"category"
+],
+[
+"Hanin er 17 år gammel og har en depression. Det har været et hårdt år for hende, og nu er hendes kæreste lige gået fra hende. <br><br>Hun lukker sig meget ind i sig selv og har svært ved at passe sin skolegang. Dog kan hun godt lide sit fag og glæder sig til at få en læreplads.",
+"<i>Kan I genkende situationen fra andre elever?<br>Har I 2-3 gode råd til, hvordan man bedst støtter elever med depression?<br>Hvordan kan man hjælpe hende med at trives, så hun ikke dropper ud?<br><i>",
+16,
+"category"
+],
+[
+"Herluf på 25 år er tidligere gået ned med stress. Han døjer stadig med eftervirkninger såsom hjernetåge, træthed og koncentrationsbesvær. <br><br>Herluf ønsker hjælp til at strukturere sin tid bedst muligt, så han undgår at gå ned med stress igen.",
+"<i>Hvad ved du om bivirkningerne efter et stressforløb?<br>Har I 2-3 gode råd til, hvordan man bedst støtter tidligere stressramte elever?<br>Hvordan støtter man bedst elever, så de ikke går ned med stress (igen)?<br><i>",
+17,
+"category"
+],
+[
+"Pernille på 23 år lider af personlighedsforstyrrelser og har i den forbindelse meget svært ved at indgå i sociale fællesskaber. <br><br>Pernille føler sig ensom, og hun ville ønske, at hun havde nogle flere venner i klassen.",
+"<i>Hvad er 2-3 vigtige ting at vide om personlighedsforstyrrelser? <br>Hvordan kan man hjælpe elever som Pernille med at falde bedre til i klassen?<br>Kan hendes underviser gøre noget for at hjælpe hende godt på vej?<br><i>",
+18,
+"category"
+],
+[
+"Helga er en 19 årig kvinde, som bor på skolehjemmet, da hendes familie bor på Island. Helga har netop fået tilbudt SPS, da hun får hyppige angstanfald og ikke trives særligt godt. Hendes mentor føler sig magtesløs og har svært ved at finde ud af, hvordan hun kan hjælpe, da Helga ofte aflyser støttetimerne med kort varsel.",
+"<i>Hvordan kan man motivere elever som Helga til at tage imod støtten?<br>Hvad er 2-3 gode råd til mentorer i arbejdet med angst?<br>Er der nogen, mentor bør kontakte i denne situation fx kolleger, forældre?<br><i>",
+19,
+"category"
+],
+[
+"Mohammed på 20 år er ordblind og lider af slem eksamensangst. Hans første skriftlige prøve nærmer sig, og han er meget nervøs for, at han ikke består.<br><br>Mohammed har fået sin IT-rygsæk, men synes ikke at det fungerer for ham, da han har svært ved at forstå, hvordan han skal bruge hjælpemidlerne.",
+"<i>Hvordan hjælper vi elever med at få gavn af IT-hjælpemidlerne i dag?<br>Hvordan kan mentorer hjælpe ordblinde elever med eksamensangst?<br>Kan SPS-hold støtte elever med eksamensangst? Hvis ja, hvordan?<br><i>",
+20,
+"category"
+],
+[
+"Safira på 30 år har ADHD. Hun har et barn på 7 år derhjemme, som tager det meste af hendes opmærksomhed og energi. <br><br>Safira har længe ønsket sig en erhvervsfaglig uddannelse, men er blevet i tvivl om, hvorvidt det er det rigtige for hende. Hun synes, at skoleperioderne er meget hårde at komme igennem.",
+"<i>Hvad er vigtigt, når man skal støtte psykisk udfordrede elever med børn? <br>Har du et konkret eksempel, hvor støtten gik godt? Hvorfor?<br>Hvornår er det (ikke) OK, at man som mentor lader en elev melde sig ud?<br><i>",
+21,
+"category"
+],
+[
+"Storm på 19 år betragter sig som nonbinær og ønsker, at alle siger “de/dem” som pronomen i stedet for “han/ham” . Nogle af Storms klassekammerater siger alligevel “han” om Storm, hvilket gør Storm meget frustrereret og ked af det.",
+"<i>Hvad er jeres erfaringer med elever, der identificerer sig som nonbinære?<br>Hvad er 2-3 råd til at få elever som Storm til at trives socialt i klassen?<br>Hvordan kan mentor og underviser samarbejde om elever som Storm?<br><i>",
+22,
+"category"
+],
+[
+"Dimitros er 43 år gammel og har autisme. Han har flere års erfaringer fra forskellige jobs, men er startet på uddannelsen efter en periode som arbejdsløs.<br><br>På skolen sidder han ofte for sig selv med hovedtelefoner på, og det er svært at få øjenkontakt med ham. I støttetimerne siger han ikke meget.",
+"<i>Hvad er Dimitros udfordring? <br>Hvornår og hvordan støtter mentorer bedst elever som Dimitros?<br>Hvornår er det OK, at elever IKKE indgår i de sociale fællesskaber?<br><i>",
+23,
+"category"
+],
+[
+"Aneta er 24 år. Hun har diagnosen OCD, men har ikke fortalt om det til andre elever. <br><br>Flere fra klassen har dog bemærket, at hun ofte vasker sine hænder mange gange om dagen. Hun savner at være en del af fællesskabet i klassen, men har svært ved at snakke med de andre.",
+"<i>Hvordan støtter man elever, der ikke er åbne om deres diagnose(r)?<br>Hvad kan mentor gøre i undervisningen?<br>Hvordan støtter man bedst elever med tvangshandlinger eller lignende?<br><i>",
+24,
+"category"
+],
+[
+"Sune på 23 har autisme. Sune blev syg op til sin første mundtlige eksamen og skal nu op til sygeeksamen. <br><br>Sune er meget nervøs og er bange for, at han går helt i sort, når han skal ind til eksamen.",
+"<i>Hvad er praksis på skolen, når elever skal sygemelde sig til en eksamen?<br>Hvordan støtter man bedst elever, som er nervøse op til eksamen?<br>Kan du fortælle om et støtteforløb, hvor det virkede særligt godt? <br><i>",
+25,
+"category"
+],
+[
+"En mentor har 10 mentees i en klasse, som alle ønsker støtte til svendeprøven.<br><br>Mentoren vil gerne hjælpe dem alle, men kan ikke finde tid til at støtte alle individuelt. Han har derfor tilbudt dem at modtage støtten i hold af 5 mentees. Det er eleverne ikke meget for, og mentoren frygter, at slet ingen vil tage imod støtten.",
+"<i>Kan I genkende situationen fra virkeligheden på skolen?<br>Hvad er fordele og ulemper ved SPS i hold?<br>Hvordan kan man motivere eleverne til at prøve SPS i hold?<br><i>",
+26,
+"category"
+],
+[
+"Dortea er ny mentor og skal til at have den første opstartssamtale med en mentee. <br><br>Hun vil gerne gøre det så godt som muligt, men hun er usikker på, hvordan hun skal gribe det an.",
+"<i>Hvad er 2-3 gode råd til nye mentorer ifm. første samtale?<br>Hvad skal man have ud af første samtale? Hvad kan vente til senere?<br>Vil I anbefale, at mentor nævner private oplysninger for at skabe tillid?<br><i>",
+27,
+"category"
+],
+[
+"Mehran på 30 har autisme og ADHD. Mehran har modtaget SPS på GF2, og det er nu tid til at evaluere forløbet inden han starter op på hovedforløbet. <br><br>Mehran har tydeligvis gavn af SPS, men han har ofte svært ved selv at sætte ord på, hvad han har brug for hjælp til, ligesom koncentration ikke er hans stærke side.",
+"<i>Hvordan evaluerer man bedst et mentorforløb sammen med eleven?<br>Hvordan evaluerer man bedst forløbet og udfylder forløbsbeskrivelse sammen med elever som Mehran?<br>Mehran på 30 har autisme og ADHD. Mehran har modtaget SPS på GF2, og det er nu tid til at evaluere forløbet inden han starter op på hovedforløbet. <br><i>",
+28,
+"category"
+],
+[
+"Albert på 18 år modtager SPS, da han er ordblind og psykisk sårbar. Han fandt først ud af, at han var ordblind ifm. skolestart og har både brug for en introduktion af IT-redskaberne til ordblinde samt løbende støtte.<br><br>Hans mentor vurderer, at Albert godt kan deltage i et SPS-hold med flere elever, som også skal sættes ind i de læse- og skriveredskaberne. Albert er dog skeptisk og vil helst have al støtte individuelt.",
+"<i>Hvad er fordele og ulemper ved at bruge SPS-hold til ordblinde elever? <br>Hvordan vurderer man bedst om mentee vil få gavn af et SPS-hold?<br>I hvilke situationer er det bedst, at eleven kun får individuel støtte?<br><i>",
+29,
+"category"
+],
+[
+"Ulrikke på 27 år har generaliseret angst og har svært ved at indgå i et klassefællesskab, da store forsamlinger gør hende utryg. Hun vil gerne have hjælp, så hun kan trives bedre i skolen.<br><br>Ulrikke har kun modtaget individuel SPS, men hendes mentor overvejer at give SPS i undervisningen for at få et bedre indtryk af Ulrikke i klassen og klassedynamikken generelt.",
+"<i>Hvordan bruger I SPS i undervisningen i dag? Hvad virker godt/skidt?<br>Hvordan samabejder mentor med underviser? Hvad må deles om eleven?<br>Hvordan ville du være med i undervisningen, hvis du var Ulrikkes mentor?<br><i>",
+30,
+"category"
+],
+[
+"Lukas er 24 år, har autisme og er ordblind. Han bryder sig ikke om at være på sin læreplads og melder sig ofte syg.Lukas har kontaktet sin mentor og fortalt ham dette, men hans mentor er i tvivl om, hvordan han skal håndtere det.",
+"<i>Har I før haft elever, som ikke trives på deres læreplads?<br>Hvordan håndterer man som mentor bedst, når mentees ikke trives der?<br>Skal mentor inddrage lærepladsen i indsigterne? Hvis ja, hvordan?<br><i>",
+31,
+"category"
+],
+[
+"Ahmed på 22 modtager SPS fordi han har psykiske vanskeligheder, men på det seneste har han haft det så godt, at han har aflyst støttetimerne. <br><br>Hans mentor overvejer derfor om Ahmed overhovedet har brug for SPS, men han er i tvivl om, hvordan man finder ud af det og taler om det med Ahmed.",
+"<i>Hvordan vil I vurdere, hvorvidt en elev ikke længere skal modtage SPS?<br>Hvordan tager man samtalen om evt. udfasning af støtten?<br>Er det muligt at skifte mentor, hvis der ikke er god “kemi”? Hvordan?<br><i>",
+32,
+"category"
+],
+[
+"Alice på 25 år er psykisk sårbar og skal til at starte på hovedforløbet. Alice udtrykker stor bekymring for det, fordi hun frygter at skulle ud på en læreplads.Alices mentor er lidt usikker på, hvordan hun skal håndtere Alices frygt for at komme ud på en læreplads.",
+"<i>Hvad er den nuværende praksis, når elever skal starte på en læreplads?<br>Hvordan kan man som mentor støtte elever som frygter lærepladsen?<br>Hvordan kan mentor samarbejde med en læreplads om elever som Alice?<br><i>",
+33,
+"category"
+],
+[
+"Katrine er 47 år, har to skolebørn og bor med sin mand. Hun har mange års arbejdserfaring som sagsbehandler i en kommune, men blev sygemeldt med stress og valgte at starte på gartneruddannelsen.<br><br>Hun er glad for undervisningen, men føler sig ikke godt tilpas socialt i klassen. Eleverne er søde, men de siger hende ikke noget socialt. Det begynder at påvirke hendes engagement i timerne.",
+"<i>Kan du genkende situationen? Hvad er problemet her? <br>Hvordan kan man støtte Katrine, så hun trives og gennemfører? <br>Kan hun bruge sit personlige netværk uden for skolen til noget her? Hvad?<br><i>",
+34,
+"category"
+]
 ];
