1bcc46ad79d4b25eb2b1d5ba8434bcae08b7db36
jdekok <30804973+jdekok@users.noreply.github.com>
2024-02-09 13:18:08 +0100

    changed rest of logic to use array from csv instead of cards.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ var currentCardContext = "";
 var currentCardQuestion = "";
 var randomcard = "";
 var randomcolor = "";
-var path_to_cards_csv = 'cards.csv';
-let cards = []; // This will hold the CSV data once loaded
+var pathToCardsCsv = 'cards.csv';
+let parsedCards = []; // This will hold the CSV data once loaded
 
 async function loadAndParseCards(path) {
 	try {
-	  const response = await fetch(path_to_cards_csv);
+	  const response = await fetch(path);
 	  const csvText = await response.text();
 	  const dataPromise = new Promise((resolve, reject) => {
 		Papa.parse(csvText, {
@@ -21,79 +21,80 @@ async function loadAndParseCards(path) {
 		  }
 		});
 	  });
-	  cards = await dataPromise; // Assign the parsed data to the global variable
+	  parsedCards = await dataPromise; // Assign the parsed data to the global variable
 	} catch (error) {
 	  console.error('Error loading or parsing cards.csv:', error);
 	}
   }
   
   loadAndParseCards(path_to_cards_csv).then(() => {
-	console.log(cards); // Now safe to use `cards`
-	// Further logic that depends on `cards` can go here
-  });
+    console.log(parsedCards); // Ensure it's using parsedCards
 
-// var availableCards = [];
-// for (var i = 0; i < cards.length; i++) {
-// 	availableCards.push(i);
-// }
+    var availableCards = [];
+    for (var i = 0; i < parsedCards.length; i++) {
+        availableCards.push(i);
+    }
 
-// var availableColors = [];
-// for (var i = 0; i < colors.length; i++) {
-// 	availableColors.push(i);
-// }
+    var availableColors = [];
+    for (var i = 0; i < colors.length; i++) {
+        availableColors.push(i);
+    }
 
-// var lastCardShownIndex = -1;
-// var lastColorShownIndex = -1;
+    var lastCardShownIndex = -1;
+    var lastColorShownIndex = -1;
 
-// function getCard() {
-// 	if (availableCards.length === 0) {
-// 		for (var i = 0; i < cards.length; i++) {
-// 			availableCards.push(i);
-// 		}
-// 		var lastCardPosition = availableCards.indexOf(lastCardShownIndex);
-// 		if (lastCardPosition > -1) {
-// 			availableCards.splice(lastCardPosition, 1);
-// 		}
-// 	}
+    function getCard() {
+        if (availableCards.length === 0) {
+            for (var i = 0; i < parsedCards.length; i++) {
+                availableCards.push(i);
+            }
+            var lastCardPosition = availableCards.indexOf(lastCardShownIndex);
+            if (lastCardPosition > -1) {
+                availableCards.splice(lastCardPosition, 1);
+            }
+        }
 
-// 	var cardIndex = Math.floor(Math.random() * availableCards.length);
-// 	randomcard = availableCards[cardIndex];
-// 	lastCardShownIndex = randomcard;
-// 	availableCards.splice(cardIndex, 1);
+        var cardIndex = Math.floor(Math.random() * availableCards.length);
+        randomcard = availableCards[cardIndex];
+        lastCardShownIndex = randomcard;
+        availableCards.splice(cardIndex, 1);
 
-// 	if (availableColors.length === 0) {
-// 		for (var i = 0; i < colors.length; i++) {
-// 			availableColors.push(i);
-// 		}
-// 		var lastColorPosition = availableColors.indexOf(lastColorShownIndex);
-// 		if (lastColorPosition > -1) {
-// 			availableColors.splice(lastColorPosition, 1);
-// 		}
-// 	}
+        if (availableColors.length === 0) {
+            for (var i = 0; i < colors.length; i++) {
+                availableColors.push(i);
+            }
+            var lastColorPosition = availableColors.indexOf(lastColorShownIndex);
+            if (lastColorPosition > -1) {
+                availableColors.splice(lastColorPosition, 1);
+            }
+        }
 
-// 	var colorIndex = Math.floor(Math.random() * availableColors.length);
-// 	randomcolor = availableColors[colorIndex];
-// 	lastColorShownIndex = randomcolor;
-// 	availableColors.splice(colorIndex, 1);
+        var colorIndex = Math.floor(Math.random() * availableColors.length);
+        randomcolor = availableColors[colorIndex];
+        lastColorShownIndex = randomcolor;
+        availableColors.splice(colorIndex, 1);
 
-// 	currentCardContext = cards[randomcard][0];
-// 	currentCardQuestion = cards[randomcard][1];
+        // Assuming your CSV has columns named 'context' and 'questions'
+        currentCardContext = parsedCards[randomcard].context;
+        currentCardQuestion = parsedCards[randomcard].questions;
 
-// 	$(document).ready(function () {
-// 		$("#newcard").animate({ backgroundColor: colors[randomcolor] }, 500);
-// 		$(".cardbox").animate({ borderColor: colors[randomcolor] }, 500);
-// 		$("blockquote footer").animate({ color: colors[randomcolor] }, 500);
-// 		$("#cardtext").animate({ opacity: 0 }, 500, function () {
-// 			$(this).animate({ opacity: 1 }, 500);
-// 			$(this).html(
-// 				currentCardContext + "<br><h1><b>···</b></h1><br>" + currentCardQuestion
-// 			);
-// 		});
-// 	});
-// }
+        $(document).ready(function () {
+            $("#newcard").animate({ backgroundColor: colors[randomcolor] }, 500);
+            $(".cardbox").animate({ borderColor: colors[randomcolor] }, 500);
+            $("blockquote footer").animate({ color: colors[randomcolor] }, 500);
+            $("#cardtext").animate({ opacity: 0 }, 500, function () {
+                $(this).animate({ opacity: 1 }, 500);
+                $(this).html(
+                    currentCardContext + "<br><h1><b>···</b></h1><br>" + currentCardQuestion
+                );
+            });
+        });
+    }
 
-// getCard();
+    getCard();
 
-// $(document).ready(function () {
-// 	$("#newcard").on("click", getCard);
-// });
+    $(document).ready(function () {
+        $("#newcard").on("click", getCard);
+    });
+
+});
